4e21613b3eea7f3b10c10ccda7c7a04d27675758
chensj.dev <chenshijie1988@yeah.net>
2019-02-20 12:40:32 +0800

    kotlin起步

---
+++ kotlin-demo01/src/com/kotlin/demo/Demo01.kt
@@ -0,0 +1,17 @@
+package com.kotlin.demo
+
+/**
+ * @author chensj
+ * @title kotlin 起步demo
+ * @project kotlin-demo01
+ * @package com.kotlin.demo
+ * @date: 2019-02-20 12:33
+ */
+fun sum(a: Int, b: Int): Int {
+    return a + b;
+}
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println(sum(10, 12))
+}
